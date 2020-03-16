d2b7829a405c165d1d4a4e5a3f6da422ab58275d
Roman Babenko <babe-roman@yandex.ru>
2020-03-16 16:03:24 +0700

    Updated sample for data_points.

--- app/src/main/java/com/inbrain/example/MyApplication.java
+++ app/src/main/java/com/inbrain/example/MyApplication.java
@@ -1,9 +1,10 @@
 package com.inbrain.example;
 
 import android.app.Application;
-import android.text.TextUtils;
 
 import com.inbrain.sdk.InBrain;
+
+import java.util.HashMap;
 
 /**
  * Created by Alex Kravchenko on 28/07/2019.
@@ -11,15 +12,21 @@ import com.inbrain.sdk.InBrain;
 public class MyApplication extends Application {
     private static final String CLIENT_ID = "9c367c28-c8a4-498d-bf22-1f3682fc73aa"; // your client id obtained by your account manager
     private static final String CLIENT_SECRET = "90MB8WyMZyYykgs0TaR21SqCcCZz3YTTXio9FoN5o5NJ6+svp3Q2tO8pvM9CjbskCaLAog0msmVTcIigKPQw4A=="; // your client secret obtained by your account manager
-    private static final String SESSION_UID = "1f3682fc73aa"; // session uid obtained by your account manager, optional
 
     @Override
     public void onCreate() {
         super.onCreate();
-        if (TextUtils.isEmpty(SESSION_UID)) {
-            InBrain.getInstance().init(this, CLIENT_ID, CLIENT_SECRET);
-        } else {
-            InBrain.getInstance().init(this, CLIENT_ID, CLIENT_SECRET, SESSION_UID);
-        }
+        InBrain.getInstance().init(this, CLIENT_ID, CLIENT_SECRET);
+
+        //optional
+        String sessionUid = "1f3682fc73aa";
+        InBrain.getInstance().setSessionUid(sessionUid);
+
+        //optional
+        HashMap<String, String> dataPoints = new HashMap<>();
+        dataPoints.put("gender", "female");
+        dataPoints.put("age", "26");
+        InBrain.getInstance().setDataPoints(dataPoints);
+
     }
 }
